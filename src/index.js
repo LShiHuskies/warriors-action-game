cb8919bf5a230e81bee9e362bf5abf1078f3bdbe
github username <github email address>
2018-09-25 01:22:01 -0400

    updated actioncable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ActionCableProvider } from 'react-actioncable-provider';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { API_WS_ROOT } from './constants';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
+const store = createStore(reducers)
+console.log('store', store.getState())
+
+ReactDOM.render(
+  <ActionCableProvider url={API_WS_ROOT}>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  </ActionCableProvider>, document.getElementById('root'));
+    registerServiceWorker();
