cd4557a83933d05578ec064b90d5dc3235bc7b19
github username <github email address>
2018-09-25 23:59:56 -0400

    update

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -9,11 +9,11 @@
 // This link also includes instructions on opting out of this behavior.
 
 const isLocalhost = Boolean(
-  warriorbackend.herokuapp.com === 'localhost' ||
+  window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
-    warriorbackend.herokuapp.com === '[::1]' ||
+    window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
-    warriorbackend.herokuapp.com.match(
+    window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
